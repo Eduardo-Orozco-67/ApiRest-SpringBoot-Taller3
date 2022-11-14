3f5b4dab1f28523741b933bae0869c4a0e02bf40
Eduardo-Orozco-67 <eduardo.orozco66@unach.mx>
2022-11-13 19:01:33 -0600

    artistas crud

--- Proyecto_taller/src/main/java/com/app/proyecto/controller/ArtistaController.java
+++ Proyecto_taller/src/main/java/com/app/proyecto/controller/ArtistaController.java
@@ -30,4 +30,31 @@ public class ArtistaController {
         return lognegocioArtista.consultarTodoslosArtistas();
     }
 
+    @GetMapping("/artistas/nom/{nombre}")
+    public ResponseEntity<?> muestraporNombre(@PathVariable("nombre") String nom){
+        return lognegocioArtista.consultarbyNombre(nom);
+    }
+
+    @GetMapping("/artistas/{id}")
+    public ResponseEntity<Artista> localizar(@PathVariable("id") int idCat){
+        return lognegocioArtista.consultarUno(idCat);
+    }
+
+    @DeleteMapping("/artistas/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable("id") int idArt){
+        return lognegocioArtista.eliminarArtista(idArt);
+
+    }
+
+    @PostMapping("/artistas")
+    public String insertar(@RequestBody Artista objcat){
+        lognegocioArtista.insertarArtista(objcat);
+        return "El o La artista fue registrado(a) correctamente";
+    }
+
+    @PutMapping("/categorias/{id}")
+    public String actualizar(@RequestBody Artista obj, @PathVariable("id") int idArt){
+        lognegocioArtista.actualizarArtista(obj, idArt);
+        return "El o La artista fue actualizado(a) correctamente";
+    }
 }
--- Proyecto_taller/src/main/java/com/app/proyecto/service/IArtistasService.java
+++ Proyecto_taller/src/main/java/com/app/proyecto/service/IArtistasService.java
@@ -9,4 +9,14 @@ import org.springframework.http.ResponseEntity;
 
 public interface IArtistasService {
     List<Artista> consultarTodoslosArtistas();
+
+    ResponseEntity<Artista> consultarUno(int idArt);
+
+    ResponseEntity<Artista>consultarbyNombre(String nombre);
+
+    ResponseEntity<Map<String, String>> insertarArtista(Artista obj);
+
+    ResponseEntity<Map<String, String>> actualizarArtista(Artista obj, int idArt);
+
+    ResponseEntity<Map<String, String>> eliminarArtista(int idArt);
 }
